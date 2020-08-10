ff1814d160adb5d74a5dbb0fbd21416dbb2db1a2
James Bradford <j.bradford@pnnl.gov>
2020-08-10 12:27:07 -0700

    rearrage files

---
+++ content/wp-to-md/dedupe.js
@@ -0,0 +1,30 @@
+
+const matchDuplicateImgRegEx = /\d+x\d+\./ig;
+
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+
+//joining path of directory
+const directoryPath = path.join(__dirname, 'uploads');
+
+//passing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+
+    //handling error
+    if (err) return console.log('Unable to scan directory: ' + err);
+
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+
+        if (file.match(matchDuplicateImgRegEx)) {
+            fs.unlinkSync(path.join(__dirname, 'uploads', file));
+            console.log('yes', file);
+        }
+        else console.log('no', file);
+
+
+    });
+
+});
