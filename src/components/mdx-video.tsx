882c39019b844ab143a4f45fdf62e39dbbfff667
James Bradford <arniebradfo@gmail.com>
2020-10-10 18:22:40 -0700

    video links

--- src/components/mdx-video.tsx
+++ src/components/mdx-video.tsx
@@ -1,20 +1,39 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react"
 
-interface MdxVideoProps {
-    src: string
+interface MdxVideoProps extends React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement> {
+  videoName: string
 }
 
-const MdxVideo: React.FC<MdxVideoProps> = ({ src, ...props }) => {
+const MdxVideo: React.FC<MdxVideoProps> = ({ videoName, children, ...props }) => {
 
-    return (
-        <video
-            preload="metadata"
-            controls
-        >
-            <source type="video/mp4" src={src} />
-            <a href={src}>{src}</a>
-        </video>
-    )
+  const { allVideos } = useStaticQuery(graphql`
+    query {
+      allVideos: allFile(filter: {ext: {regex: "/(avi|mp4|mov|mkv|ogg)/i"}}) {
+        edges {
+          node {
+            publicURL
+            base
+          }
+        }
+      }
+    }
+  `);
+
+  // TODO: test the performance of this...
+  const { node: video } = allVideos.edges.find(edge => videoName == edge.node.base)
+
+  return (
+    <video
+      preload="metadata"
+      controls
+      {...props}
+    >
+      <source type="video/mp4" src={video.publicURL} />
+      <a href={video.publicURL}>{video.base}</a>
+      {children}
+    </video>
+  )
 }
 
 export default MdxVideo
